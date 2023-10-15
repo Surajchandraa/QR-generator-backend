4acad6a484da7bc7d16fa110f8673a3d4867b69c
suraj <surajjosh7@gmail.com>
2023-10-15 21:37:31 +0530

    updated

--- public/script.js
+++ public/script.js
@@ -31,6 +31,14 @@ function create_email(){
    main.appendChild(create);
 }
 
+function create_phone(){
+    let create = document.createElement('div');
+    create.classList.add('maincontainer');
+   create.innerHTML=`<input id="inp" type="number" placeholder="Enter phone number">
+            
+   <button class="btn2">Generate</button>`
+   main.appendChild(create);
+}
 
 
 function ip_text(){
@@ -231,6 +239,140 @@ function ip_email(){
 
 }
 
+function ip_phone(){
+    if(main.firstElementChild){
+        main.removeChild(main.firstElementChild);
+        let opp= document.querySelectorAll('.output');
+        opp.forEach(element => {
+        element.remove();
+    });
+        
+        create_phone()
+
+    }
+    else if(!main.firstElementChild){
+       create_phone()
+
+    }
+
+
+    let button = document.getElementsByClassName('btn2')[0];
+    let inputfield=document.getElementById('inp')
+    button.addEventListener("click",function(){
+        if(!inputfield.value){
+            alert("please enter an valid phone number")
+        }
+        else{
+        let output = document.createElement('div');
+        output.classList.add('output');
+
+        let dataset = {
+            type:"phone",
+            data:`tel:${inputfield.value}`
+                }
+        
+                const options = {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dataset)
+                  };
+
+
+        
+        let apidata = fetch("http://localhost:5000/generate/url",options);
+        apidata.then(function(value){
+            return value.json()
+        }).then(function(value){
+
+            output.innerHTML=`<a href=${value.qrCodeUrl} download>
+            <img id="image" src=${value.qrCodeUrl} alt="Qr code">
+            Click here to download
+        </a> <br><br>`
+        main.appendChild(output);
+
+        console.log(value)
+
+        })
+    }
+       
+})
+    
+
+   
+
+}
+
+
+
+function ip_sms(){
+    if(main.firstElementChild){
+        main.removeChild(main.firstElementChild);
+        let opp= document.querySelectorAll('.output');
+        opp.forEach(element => {
+        element.remove();
+    });
+        
+        create_phone()
+
+    }
+    else if(!main.firstElementChild){
+       create_phone()
+
+    }
+
+
+    let button = document.getElementsByClassName('btn2')[0];
+    let inputfield=document.getElementById('inp')
+    button.addEventListener("click",function(){
+        if(!inputfield.value){
+            alert("please enter an valid phone number")
+        }
+        else{
+        let output = document.createElement('div');
+        output.classList.add('output');
+
+        let dataset = {
+            type:"phone",
+            data:`smsto:${inputfield.value}`
+                }
+        
+                const options = {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dataset)
+                  };
+
+
+        
+        let apidata = fetch("http://localhost:5000/generate/url",options);
+        apidata.then(function(value){
+            return value.json()
+        }).then(function(value){
+
+            output.innerHTML=`<a href=${value.qrCodeUrl} download>
+            <img id="image" src=${value.qrCodeUrl} alt="Qr code">
+            Click here to download
+        </a> <br><br>`
+        main.appendChild(output);
+
+        console.log(value)
+
+        })
+    }
+       
+})
+    
+
+   
+
+}
+
 types[1].addEventListener("click",ip_text);
 types[0].addEventListener("click",ip_url);
 types[2].addEventListener("click",ip_email);
+types[3].addEventListener("click",ip_phone);
+types[5].addEventListener("click",ip_sms);
--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,10 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","index.html"));
+})
+
 app.post('/generate/url', (req, res) => {
     const { data } = req.body;
 
